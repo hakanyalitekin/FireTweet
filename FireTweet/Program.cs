d8492c8bc764dad2bd25673e245bd9f5d775909d
Hakan.Yalitekin  <hakan.yalitekin@lcwaikiki.com>
2022-03-04 14:26:52 +0300

    Add project files.

---
+++ FireTweet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Tweetinvi;
+
+namespace FireTweet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var _ConsumerKey = Environment.GetEnvironmentVariable("ConsumerKey");
+            var _ConsumerSecret = Environment.GetEnvironmentVariable("ConsumerSecret");
+            var _AccessToken = Environment.GetEnvironmentVariable("AccessToken");
+            var _AccessTokenSecret = Environment.GetEnvironmentVariable("AccessTokenSecret");
+
+            var userClient = new TwitterClient(_ConsumerKey, _ConsumerSecret, _AccessToken, _AccessTokenSecret);
+            var tweet = userClient.Tweets.PublishTweetAsync($"API TEST:{DateTime.Now}");
+            Console.WriteLine("Tweet Published : " + tweet);
+
+        }
+    }
+}
