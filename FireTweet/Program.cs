7cab8fcafb47e8c9376ba5196ab4a6a03b519336
Hakan.Yalitekin  <hakan.yalitekin@lcwaikiki.com>
2022-03-04 14:47:58 +0300

    bug fix

--- FireTweet/Program.cs
+++ FireTweet/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Tweetinvi;
 
 namespace FireTweet
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var _ConsumerKey = Environment.GetEnvironmentVariable("ConsumerKey");
             var _ConsumerSecret = Environment.GetEnvironmentVariable("ConsumerSecret");
@@ -13,7 +14,7 @@ namespace FireTweet
             var _AccessTokenSecret = Environment.GetEnvironmentVariable("AccessTokenSecret");
 
             var userClient = new TwitterClient(_ConsumerKey, _ConsumerSecret, _AccessToken, _AccessTokenSecret);
-            var tweet = userClient.Tweets.PublishTweetAsync($"API TEST:{DateTime.Now}");
+            var tweet = await userClient.Tweets.PublishTweetAsync($"API TEST:{DateTime.Now}");
             Console.WriteLine("Tweet Published : " + tweet);
 
         }
